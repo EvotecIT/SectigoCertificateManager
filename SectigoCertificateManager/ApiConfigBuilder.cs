d3b7ef3f22a06053d4d6cdb5540e5bf0e8e2698e
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-03 10:45:51 +0200

    Add fluent configuration support

---
+++ SectigoCertificateManager/ApiConfigBuilder.cs
@@ -0,0 +1,132 @@
+namespace SectigoCertificateManager;
+
+
+
+/// <summary>
+
+/// Provides a builder for creating instances of <see cref="ApiConfig"/> using a fluent API.
+
+/// </summary>
+
+public sealed class ApiConfigBuilder
+
+{
+
+    private string _baseUrl = string.Empty;
+
+    private string _username = string.Empty;
+
+    private string _password = string.Empty;
+
+    private string _customerUri = string.Empty;
+
+    private ApiVersion _apiVersion = ApiVersion.V25_4;
+
+
+
+    /// <summary>
+
+    /// Sets the base URL for the API endpoint.
+
+    /// </summary>
+
+    /// <param name="baseUrl">Base URL of the API.</param>
+
+    /// <returns>The builder instance.</returns>
+
+    public ApiConfigBuilder WithBaseUrl(string baseUrl)
+
+    {
+
+        _baseUrl = baseUrl;
+
+        return this;
+
+    }
+
+
+
+    /// <summary>
+
+    /// Sets the credentials used for authentication.
+
+    /// </summary>
+
+    /// <param name="username">User name.</param>
+
+    /// <param name="password">Password.</param>
+
+    /// <returns>The builder instance.</returns>
+
+    public ApiConfigBuilder WithCredentials(string username, string password)
+
+    {
+
+        _username = username;
+
+        _password = password;
+
+        return this;
+
+    }
+
+
+
+    /// <summary>
+
+    /// Sets the customer URI header value.
+
+    /// </summary>
+
+    /// <param name="customerUri">Customer URI value.</param>
+
+    /// <returns>The builder instance.</returns>
+
+    public ApiConfigBuilder WithCustomerUri(string customerUri)
+
+    {
+
+        _customerUri = customerUri;
+
+        return this;
+
+    }
+
+
+
+    /// <summary>
+
+    /// Sets the API version.
+
+    /// </summary>
+
+    /// <param name="version">Version of the API.</param>
+
+    /// <returns>The builder instance.</returns>
+
+    public ApiConfigBuilder WithApiVersion(ApiVersion version)
+
+    {
+
+        _apiVersion = version;
+
+        return this;
+
+    }
+
+
+
+    /// <summary>
+
+    /// Builds a new <see cref="ApiConfig"/> instance using configured values.
+
+    /// </summary>
+
+    /// <returns>An <see cref="ApiConfig"/>.</returns>
+
+    public ApiConfig Build()
+
+        => new ApiConfig(_baseUrl, _username, _password, _customerUri, _apiVersion);
+
+}
+
