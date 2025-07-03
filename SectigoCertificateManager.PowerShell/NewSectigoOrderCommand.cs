82b03d36383c04741012aa9bc9cc64acede36aff
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-03 14:13:53 +0200

    Add PowerShell cmdlets

---
+++ SectigoCertificateManager.PowerShell/NewSectigoOrderCommand.cs
@@ -0,0 +1,54 @@
+using System.Management.Automation;
+using SectigoCertificateManager;
+using SectigoCertificateManager.Clients;
+using SectigoCertificateManager.Requests;
+
+namespace SectigoCertificateManager.PowerShell;
+
+[Cmdlet(VerbsCommon.New, "SectigoOrder")]
+[OutputType(typeof(Models.Certificate))]
+public sealed class NewSectigoOrderCommand : PSCmdlet
+{
+    [Parameter(Mandatory = true)]
+    public string BaseUrl { get; set; } = string.Empty;
+
+    [Parameter(Mandatory = true)]
+    public string Username { get; set; } = string.Empty;
+
+    [Parameter(Mandatory = true)]
+    public string Password { get; set; } = string.Empty;
+
+    [Parameter(Mandatory = true)]
+    public string CustomerUri { get; set; } = string.Empty;
+
+    [Parameter]
+    public ApiVersion ApiVersion { get; set; } = ApiVersion.V25_4;
+
+    [Parameter(Mandatory = true)]
+    public string CommonName { get; set; } = string.Empty;
+
+    [Parameter(Mandatory = true)]
+    public int ProfileId { get; set; }
+
+    [Parameter]
+    public int Term { get; set; } = 12;
+
+    [Parameter]
+    public string[] SubjectAlternativeName { get; set; } = System.Array.Empty<string>();
+
+    protected override void ProcessRecord()
+    {
+        var config = new ApiConfig(BaseUrl, Username, Password, CustomerUri, ApiVersion);
+        var client = new SectigoClient(config);
+        var certificates = new CertificatesClient(client);
+        var request = new IssueCertificateRequest
+        {
+            CommonName = CommonName,
+            ProfileId = ProfileId,
+            Term = Term,
+            SubjectAlternativeNames = SubjectAlternativeName
+        };
+        var certificate = certificates.IssueAsync(request).GetAwaiter().GetResult();
+        WriteObject(certificate);
+    }
+}
