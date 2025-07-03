277e6fb719239e4ed19139a2784751ab386c1d82
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-03 14:14:01 +0200

    Refactor examples into separate class

--- SectigoCertificateManager.Examples/Program.cs
+++ SectigoCertificateManager.Examples/Program.cs
@@ -1,4 +1,3 @@
-using SectigoCertificateManager;
-// See https://aka.ms/new-console-template for more information
-var obj = new Class1();
-Console.WriteLine($"Example using {obj.Name}");
+using SectigoCertificateManager.Examples.Examples;
+
+await BasicApiExample.RunAsync();
