c033d7a8d39da640765baeac76e5d7d5cbeb2612
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-03 09:06:42 +0200

    Add multi-target projects with examples, CLI, PowerShell and tests

---
+++ SectigoCertificateManager.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using SectigoCertificateManager;
+using Xunit;
+
+namespace SectigoCertificateManager.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var obj = new Class1();
+        Assert.Equal("Class1", obj.Name);
+    }
+}
