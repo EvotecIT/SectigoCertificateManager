41287240a230a1445eaa70f8df8303bfef8156b2
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-04 09:33:22 +0200

    refactor: split exception classes into separate files

---
+++ SectigoCertificateManager/ValidationException.cs
@@ -0,0 +1,12 @@
+namespace SectigoCertificateManager;
+
+/// <summary>
+/// Exception thrown when a request fails validation.
+/// </summary>
+public sealed class ValidationException : ApiException {
+    /// <summary>Initializes a new instance of the <see cref="ValidationException"/> class.</summary>
+    /// <param name="error">Error information returned by the API.</param>
+    public ValidationException(ApiError error)
+        : base(error) {
+    }
+}
