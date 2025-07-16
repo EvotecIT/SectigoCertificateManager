0289f989d422192fe0ba1b28d91113da157d8464
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-16 12:38:55 +0200

    feat(module): ✨ Add initial module structure and functionality
    * Introduced new PowerShell module `SectigoCertificateManager` with core cmdlets.
    * Implemented `OnModuleImportAndRemove` class for assembly resolution.
    * Added `FUNDING.yml` and `dependabot.yml` for funding and dependency management.
    * Created build scripts for package publishing and version updates.
    * Established test framework with `SectigoCertificateManager.Tests.ps1`.

---
+++ Build/UpdateVersion.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module PSPublishModule -Force
+
+Get-ProjectVersion -Path "C:\Support\GitHub\SectigoCertificateManager" -ExcludeFolders @('C:\Support\GitHub\SectigoCertificateManager\Module\Artefacts') | Format-Table
+
+Set-ProjectVersion -Path "C:\Support\GitHub\SectigoCertificateManager" -NewVersion "0.5.0" -Verbose -ExcludeFolders @('C:\Support\GitHub\SectigoCertificateManager\Module\Artefacts')
