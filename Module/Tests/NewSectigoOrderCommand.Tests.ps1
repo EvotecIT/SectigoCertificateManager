1359ef55cb2b21a72cd50921c03e23a805400acc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-18 10:35:44 +0200

    fix(powershell): 🐛 validate common name

--- Module/Tests/NewSectigoOrderCommand.Tests.ps1
+++ Module/Tests/NewSectigoOrderCommand.Tests.ps1
@@ -11,4 +11,16 @@ Describe 'New-SectigoOrder parameter validation' -Tag 'Cmdlet' {
         }
         { New-SectigoOrder @params } | Should -Throw
     }
+
+    It 'Throws when CommonName is null or whitespace' {
+        $params = @{
+            BaseUrl  = 'https://example.com'
+            Username = 'user'
+            Password = 'pass'
+            CustomerUri = 'cust'
+            CommonName = ' '
+            ProfileId = 1
+        }
+        { New-SectigoOrder @params } | Should -Throw
+    }
 }
