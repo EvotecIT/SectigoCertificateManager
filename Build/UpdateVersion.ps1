26dd50644a861d129db20610d492bd17cb08fce0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-12-02 08:24:01 +0100

    chore: ✨ Update project version to `0.1.0` and enhance project metadata
    * Updated `Set-ProjectVersion` in `UpdateVersion.ps1` to reflect new version.
    * Enhanced project file `SectigoCertificateManager.csproj` with additional metadata including:
      - Description
      - Target frameworks
      - Package information
    * Added license and repository details to `README.md`.

--- Build/UpdateVersion.ps1
+++ Build/UpdateVersion.ps1
@@ -2,4 +2,4 @@
 
 Get-ProjectVersion -Path "C:\Support\GitHub\SectigoCertificateManager" -ExcludeFolders @('C:\Support\GitHub\SectigoCertificateManager\Module\Artefacts') | Format-Table
 
-Set-ProjectVersion -Path "C:\Support\GitHub\SectigoCertificateManager" -NewVersion "0.5.0" -Verbose -ExcludeFolders @('C:\Support\GitHub\SectigoCertificateManager\Module\Artefacts')
+Set-ProjectVersion -Path "C:\Support\GitHub\SectigoCertificateManager" -NewVersion "0.1.0" -Verbose -ExcludeFolders @('C:\Support\GitHub\SectigoCertificateManager\Module\Artefacts')
