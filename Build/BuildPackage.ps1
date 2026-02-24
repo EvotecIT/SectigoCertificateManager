220c22011737da4a3997237cd0414b572f4b6f81
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-02-24 08:37:39 +0100

    refactor(build): 🔨 standardize build pipeline to Build-Project + project.build.json

--- Build/BuildPackage.ps1
+++
@@ -1,3 +0,0 @@
-﻿Clear-Host
-
-Invoke-DotNetReleaseBuild -ProjectPath "$PSScriptRoot\..\SectigoCertificateManager" -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
--- Build/PublishPackageNuget.ps1
+++
@@ -1,4 +0,0 @@
-﻿Import-Module PSPublishModule -Force -ErrorAction Stop
-
-$NugetAPI = Get-Content -Raw -LiteralPath "C:\Support\Important\NugetOrgEvotec.txt"
-Publish-NugetPackage -Path "$PSScriptRoot\..\SectigoCertificateManager\bin\Release" -ApiKey $NugetAPI
